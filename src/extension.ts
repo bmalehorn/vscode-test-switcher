2ab2619f089519934e13f182f76fd9449f8bd908
Brian Malehorn <bmalehorn@gmail.com>
2019-07-12 21:53:40 -0700

    it works! Switched in rails app

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as fs from "fs";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,14 +14,49 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World!");
-  });
+  let disposable = vscode.commands.registerCommand(
+    "test-switcher.switch",
+    doSwitch,
+  );
 
   context.subscriptions.push(disposable);
+}
+
+export interface Rule {
+  pattern: string;
+  replacement: string;
+}
+
+const rules: Rule[] = [
+  { pattern: "(\\w+)\\.tsx", replacement: "__tests__/$1.test.tsx" },
+  { pattern: "app/models/(\\w+)\\.rb", replacement: "spec/models/$1_spec.rb" },
+];
+
+export function match(path: string, rule: Rule): string | undefined {
+  const { pattern, replacement } = rule;
+  const regex = new RegExp(pattern);
+  if (!path.match(regex)) {
+    return;
+  }
+  const replaced = path.replace(regex, replacement);
+  return replaced;
+}
+
+async function doSwitch() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+
+  const fileName = editor.document.fileName;
+  for (const rule of rules) {
+    const related = match(fileName, rule);
+    if (related && fs.existsSync(related)) {
+      const document = await vscode.workspace.openTextDocument(related);
+      await vscode.window.showTextDocument(document);
+      return;
+    }
+  }
 }
 
 // this method is called when your extension is deactivated
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -9,7 +9,7 @@ import * as assert from "assert";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 // import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+import { Rule, match } from "../extension";
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function() {
@@ -17,5 +17,35 @@ suite("Extension Tests", function() {
   test("Something 1", function() {
     assert.equal(-1, [1, 2, 3].indexOf(5));
     assert.equal(-1, [1, 2, 3].indexOf(0));
+  });
+
+  test("match basic", function() {
+    const rule: Rule = { pattern: "in", replacement: "in-test" };
+    assert.equal(match("xxx", rule), undefined);
+    assert.equal(match("in", rule), "in-test");
+  });
+
+  test("match replacement 1", function() {
+    assert.equal(
+      match("in", { pattern: "(in)", replacement: "$1.test" }),
+      "in.test",
+    );
+  });
+
+  test("match replacement 2", function() {
+    assert.equal(
+      match("ab", { pattern: "a(b)", replacement: "$1.test" }),
+      "b.test",
+    );
+  });
+
+  test("match replacement rails", () => {
+    assert.equal(
+      match("/home/brian/sample_app_rails_4/app/models/micropost.rb", {
+        pattern: "app/models/(\\w+)\\.rb",
+        replacement: "spec/models/$1_spec.rb",
+      }),
+      "/home/brian/sample_app_rails_4/spec/models/micropost_spec.rb",
+    );
   });
 });
