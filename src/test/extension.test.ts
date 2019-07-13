bdf2c99236a0d51adb30548d393a291d1037e2e3
Brian Malehorn <bmalehorn@gmail.com>
2019-07-13 11:14:32 -0700

    update packages

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,18 +1,18 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from "assert";
+import { before } from "mocha";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
+import * as vscode from "vscode";
 // import * as vscode from 'vscode';
 import { Rule, match, DEFAULT_RULES } from "../extension";
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
+  before(() => {
+    vscode.window.showInformationMessage("Start all tests.");
+  });
+
   // Defines a Mocha unit test
   test("Something 1", () => {
     assert.equal(-1, [1, 2, 3].indexOf(5));
